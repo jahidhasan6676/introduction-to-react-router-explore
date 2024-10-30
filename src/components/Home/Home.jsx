5e5e8ceb424d7cb01cf2fcdf1b85e5d1269ff3f9
jahidhasan6676 <jh18186676@gmail.com>
2024-10-30 18:19:34 +0600

    added the full introduction react router

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+    const navigation = useNavigation();
+    const location = useLocation();
+    console.log(location)
     return (
         <div>
             <Header></Header>
+            {
+                navigation.state === "loading"? <p>Loading...</p>:<Outlet></Outlet>
+            }
             <h2>Welcome to my website</h2>
-            <Outlet></Outlet>
+            
             <Footer></Footer>
         </div>
     );
